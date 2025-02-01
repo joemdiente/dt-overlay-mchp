673b1f65d1e0f3e95b76f4288653d0b8333d3343
Joemel John Diente <joemdiente@gmail.com>
2025-02-01 23:36:42 +0800

    mdio_gpio_eds: working its and dtso

--- transfer_to_build.sh
+++ transfer_to_build.sh
@@ -10,19 +10,4 @@ echo "[Transfer Script] Copy new files to build directory"
 sudo rsync -avrP ./* $BUILD_DT_OVERLAY_DIR
 echo "[Transfer Script] ....Done...."
 export BUILD_DT_OVERLAY_DIR=$BUILD_DT_OVERLAY_DIR
-ls -lh $BUILD_DT_OVERLAY_DIR/sama5d3_eds | grep .dtbo
-
-		# fdt_iocon1_mdio_gpio {
-		# 	description = "Device Tree blob for mdio-gpio in IOCON1";
-		# 	data = /incbin/("./sama5d3_eds/sama5d3_eds_mdio_gpio.dtbo");
-		# 	type = "flat_dt";
-		# 	arch = "arm";
-		# 	compression = "none";
-		# 	load = <0x23152000>;
-		# 	hash-1 {
-		# 		algo = "crc32";
-		# 	};
-		# 	hash-2 {
-		# 		algo = "sha1";
-		# 	};
-		# };
+# ls -lh $BUILD_DT_OVERLAY_DIR/sama5d3_eds | grep .dtbo
